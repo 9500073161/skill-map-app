7a1355bc83e2da2087dd870733d86ef084702132
Prashob <89034675+9500073161@users.noreply.github.com>
2024-03-16 11:16:02 +0100

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Page is under construction",
+			"message": "The Page is under construction",
 		})
 	})
 
