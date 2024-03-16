8f965453c01e2b120e0bea9a7c5d439696d4b918
Prashob <89034675+9500073161@users.noreply.github.com>
2024-03-16 08:16:19 +0100

    SKILLMAP-4-added GIN
    Added GIN package and local server

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Developing Skill-MAP")
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Page is under construction",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
